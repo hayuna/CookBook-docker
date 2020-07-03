620a0ca734b3ca2aebff46a2263cfecd22527524
Hayuna <marek.kowalonek@gmail.com>
2020-07-03 23:23:43 +0200

    refactor

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const user = require("routes/user.route");
-const music = require("routes/music.route");
 const dish = require("routes/dish.route");
 const ingredient = require("routes/ingredient.route");
 const cloudinary = require("routes/cloudinary.route");
 
-router.use("/users", user);
-router.use("/musics", music);
 router.use("/dish", dish);
 router.use("/ingredient", ingredient);
 router.use("/cloudinary", cloudinary);
